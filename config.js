735d3d48fa48a0237ec4b3938beacb3311136f41
avoivo <tvoivodas@gmail.com>
2021-04-16 09:24:18 +0300

    3 files modified

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const config = {
+const config = {
   quizUrl: "https://proto.io/en/jobs/candidate-exercise/quiz.json",
   resultUrl: "https://proto.io/en/jobs/candidate-exercise/result.json"
 };
--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@ const appEl = document.getElementById("app");
 const templatePresenter = new TemplatePresenter(appEl);
 
 templatePresenter.showSpinner();
+
+fetch(config.quizUrl)
+  .then(response => response.json())
+  .then(data => {
+    console.log("Data Loaded");
+    // this.quizData = data;
+    // const quizIntro = new QuizIntro(data, this.htmlUtils);
+    // this.highestScore = quizIntro.highestPosibleScore();
+    // const firstPage = quizIntro.create(() => this.navigateToNextQuestion());
+
+    // this.navigator.navigate(firstPage);
+  })
+  .catch(error => {
+    console.error(error);
+    // this.navigator.navigate(this.htmlUtils.createError(error));
+  });

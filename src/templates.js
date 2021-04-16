f505e978d61e65cbeb41a289a14c347587d789e1
avoivo <tvoivodas@gmail.com>
2021-04-16 11:41:24 +0300

    3 files modified, 1 deleted

--- src/templates.js
+++ src/templates.js
@@ -2,24 +2,46 @@ const Templates = {
   spinner: document.getElementById("spinner-template"),
   question: document.getElementById("question-template"),
   result: document.getElementById("result-template"),
-  error: document.getElementById("error-template")
+  error: document.getElementById("error-template"),
+  radioButton: document.getElementById("radiobutton-template"),
+  checkBox: document.getElementById("checkbox-template")
 };
 
 class TemplatePresenter {
   constructor(app) {
     this.app = app;
+    this.lastDisplayed;
+  }
+
+  present(element) {
+    if (!!!element) {
+      return;
+    }
+
+    if (this.lastDisplayed === undefined) {
+      this.app.appendChild(element);
+    } else {
+      this.app.replaceChild(element, this.lastDisplayed);
+    }
+
+    this.lastDisplayed = element;
   }
 
   showSpinner() {
-    this.app.appendChild(Templates.spinner.content.cloneNode(true));
+    this.present(Templates.spinner.content.cloneNode(true));
   }
 
   showQuestion(quizTitle, quizDescription, question) {
-
-
+    const createCheckBox = (id, text) => {
+      const checkBoxElement = Templates.checkBox.content.cloneNode(true);
+      checkBoxElement.dataset.id = id;
+      const inputElement = checkBoxElement.querySelector("input");
+      inputElement.id = id;
+      inputElement.name = id;
+      return checkBoxElement;
+    };
+    debugger;
     const questionElement = Templates.question.content.cloneNode(true);
-
-    this.app.appendChild(questionElement);
 
     const questionTitleElement = document.querySelector(".quiz-title");
     questionTitleElement.textContent = quizTitle;
@@ -28,17 +50,20 @@ class TemplatePresenter {
       ".quiz-description"
     );
     questionDescriptionElement.textContent = quizDescription;
+
+    const answersContainerElement = questionElement.querySelector(
+      ".answers-container"
+    );
+    this.present(questionElement);
   }
 
   showResult() {
-    this.app.appendChild(Templates.spinner.content.cloneNode(true));
-    this.app.appendChild(Templates.intro.content.cloneNode(true));
+    this.present(Templates.spinner.content.cloneNode(true));
+    this.present(Templates.intro.content.cloneNode(true));
   }
 
   showError(error) {
-    const errorContainer = Templates.error.content.cloneNode(true);
-    this.app.appendChild(errorContainer);
-    const errorTextEl = document.querySelector(".error-text");
-    errorTextEl.textContent = error;
+    Templates.error.content.querySelector(".error-text").textContent = error;
+    this.present(Templates.error.content.cloneNode(true));
   }
 }
